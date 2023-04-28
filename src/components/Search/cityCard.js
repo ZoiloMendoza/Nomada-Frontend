4ab291e582dd7f966de1b35303f065a4919a97f0
PaolaBramlett <paolabramlett@gmail.com>
2023-04-28 05:01:37 -0600

    p. creando pagina search

---
+++ src/components/Search/cityCard.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardMedia, CardContent, Typography, IconButton } from '@material-ui/core';
+import { Favorite, Add } from '@material-ui/icons';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: '1rem',
+    position: 'relative',
+    transition: 'transform 0.3s',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    width: '50%',
+    margin: 'auto',
+    transition: 'width 0.3s',
+    '&:hover': {
+      width: '100%',
+    },
+  },
+  content: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    color: 'white',
+    width: '100%',
+    padding: '1rem',
+    transition: 'opacity 0.3s',
+    opacity: 0,
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+});
+
+const CityCard = ({ cityData }) => {
+  const classes = useStyles();
+  const [hovered, setHovered] = useState(false);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const handleClick = () => {
+    console.log('Add button clicked!');
+  };
+
+  const handleFavoriteClick = () => {
+    console.log('Favorite button clicked!');
+  };
+
+  return (
+    <>
+      <h2>Destinos</h2>
+      <Slider {...settings}>
+        {cityData.map((cityData) => (
+          <Card
+            className={classes.card}
+            key={cityData.id}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <CardMedia component='img' className={classes.media} image={cityData.image} title={cityData.title} />
+            <CardContent className={classes.content}>
+              {hovered && (
+                <>
+                  <Typography className={classes.title} variant='h5' gutterBottom>
+                    {cityData.title}
+                  </Typography>
+                  <Typography className={classes.subtitle} variant='subtitle1' gutterBottom>
+                    {cityData.subtitle}
+                  </Typography>
+
+                  <IconButton className={classes.iconButton} aria-label='add' onClick={handleClick}>
+                    <Add />
+                  </IconButton>
+                  <IconButton className={classes.iconButton} aria-label='favorite' onClick={handleFavoriteClick}>
+                    <Favorite />
+                  </IconButton>
+                </>
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </Slider>
+    </>
+  );
+};
+
+export default CityCard;
---
+++ src/components/Search/cityData.js
@@ -0,0 +1,54 @@
+export const cityData = [
+  {
+    id: 1,
+    title: 'Holbox',
+    subtitle: 'Quintana Roo',
+    location: 'bar',
+    image:
+      'https://img.itinari.com/activity/images/original/d3c6fdbe-962a-416b-85d9-e514c4a56653-istock-1190233703.jpg?ch=DPR&dpr=2.625&w=1200&h=800&s=518354b88e6b7dd37ebdb07afb5faa23',
+    rating: '5',
+  },
+  {
+    id: 2,
+    title: 'Cozumel',
+    subtitle: 'Quintana Roo',
+    location: 'bar',
+    image:
+      'https://destinationlesstravel.com/wp-content/uploads/2022/05/A-birds-eye-view-of-the-port-in-Cozumel-Mexico.jpg.webp',
+    rating: '5',
+  },
+  {
+    id: 3,
+    title: 'Playa del Carmen',
+    subtitle: 'Quintana Roo',
+    location: 'bar',
+    image:
+      'https://images.squarespace-cdn.com/content/v1/545ece7fe4b0edd3bdd2dbdf/1626476545543-EQB38HEJ3DS2AATAQ28U/Playa+del+Carmen+Fifth+Avenue.jpg',
+    rating: '5',
+  },
+  {
+    id: 4,
+    title: 'Bacalar',
+    subtitle: 'Quintana Roo',
+    location: 'bar',
+    image: 'https://www.velerobacalar.com.mx/wp-content/uploads/2022/02/bacalar-laguna-7-colores_mexico.webp',
+    rating: '5',
+  },
+  {
+    id: 5,
+    title: 'Isla Mujeres',
+    subtitle: 'Quintana Roo',
+    location: 'bar',
+    image: 'https://www.mexperience.com/wp-content/uploads/Isla-Mujeres-North-Beach-Aerial-NBS.jpg',
+    rating: '5',
+  },
+  {
+    id: 6,
+    title: 'Mérida',
+    subtitle: 'Yucatán',
+    location: 'bar',
+    image:
+      'https://escapadas.mexicodesconocido.com.mx/wp-content/uploads/2020/10/Depositphotos_Plaza-Grande-Merida-Tucatan-centro-historico-scaled.jpg',
+    rating: '5',
+  },
+];
