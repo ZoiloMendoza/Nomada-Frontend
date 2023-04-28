7f0a27e3d15c37fee8793c6944a4510490e3385e
RA-1124 <alison.rangelperez@gmail.com>
2023-04-27 20:12:55 -0600

    A cambios formularios

--- src/components/Registro/formulario.js
+++ src/components/Registro/formulario.js
@@ -2,8 +2,23 @@ import { Box, TextField, Grid, Card, CardContent } from '@mui/material';
 import CheckboxFormulario from './Checkbox';
 import ButtonForm from './ButtonForm';
 import BoxRegistro from './BoxRegistro';
+import { useState } from 'react';
 
 function Formulario() {
+  const [formData, setFormData] = useState({
+    usuario: '',
+    correo: '',
+    contraseña: '',
+  });
+  const { usuario, correo, contraseña } = formData;
+  const handleOnChange = (e) => {
+    console.log([e.target.name], e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (usuario, correo, contraseña) => {
+    alert('datos formulario', usuario.correo, contraseña);
+  };
   return (
     <div className='Formulario'>
       <header className='Form-header'>
@@ -19,6 +34,8 @@ function Formulario() {
                     label='usuario'
                     type='text'
                     name='usuario'
+                    value={formData.usuario}
+                    onChange={handleOnChange}
                     margin='dense'
                     fullWidth
                     variant='outlined'
@@ -32,6 +49,8 @@ function Formulario() {
                         label='correo electronico'
                         type='text'
                         name='correo electronico'
+                        value={formData.correo}
+                        onChange={handleOnChange}
                         margin='dense'
                         fullWidth
                         variant='outlined'
@@ -43,8 +62,10 @@ function Formulario() {
                           <TextField
                             error={false}
                             label='contraseña'
-                            type='text'
+                            type='password'
                             name='contraseña'
+                            value={formData.contraseña}
+                            onChange={handleOnChange}
                             margin='dense'
                             fullWidth
                             variant='outlined'
@@ -56,7 +77,7 @@ function Formulario() {
                               <TextField
                                 error={false}
                                 label='confirmar contraseña'
-                                type='text'
+                                type='password'
                                 name='confirmar contraseña'
                                 margin='dense'
                                 fullWidth
@@ -64,7 +85,7 @@ function Formulario() {
                               />
                             </CardContent>
                             <CheckboxFormulario />
-                            <ButtonForm />
+                            <ButtonForm size='medium' onClick={() => handleSubmit(usuario, correo, contraseña)} />
                           </Card>
                         </Grid>
                       </Card>
