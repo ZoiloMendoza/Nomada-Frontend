f4e1f0305b6b80a2b1a43b552efe0a4f9ac33d11
ZoiloMendoza <zoilomm@hotmail.com>
2023-05-03 16:25:54 -0600

    uso de variables de entorno

--- src/pages/crear-viaje.js
+++ src/pages/crear-viaje.js
@@ -1,6 +1,7 @@
 import BoardingPassCardWrapper from '@/components/CrearViaje/CrearViaje';
 import Head from 'next/head';
 import styles from '@/styles/Crear-viaje.module.css';
+
 export default function Crear() {
   return (
     <>
