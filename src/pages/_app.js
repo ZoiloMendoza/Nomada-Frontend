b3473afa94fd879b8e54850ee7bba1dd683b3501
ZoiloMendoza <zoilomm@hotmail.com>
2023-05-13 00:54:09 -0600

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,34 +1,22 @@
-import { StylesProvider, createGenerateClassName } from '@mui/styles';
-
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-config.autoAddCss = false;
-//import '@/styles/globals.css';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2B2E4A', // Coloca aquí el color principal que desees
+      main: '#2B2E4A',
     },
     secondary: {
-      main: '#FFC107', // Coloca aquí el color secundario que desees
+      main: '#FFC107',
     },
   },
 });
 
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'c',
-});
-
 export default function App({ Component, pageProps }) {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </StylesProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
