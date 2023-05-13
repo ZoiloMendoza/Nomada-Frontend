5ca5a7f6f6498b2f29dc28b682913da89137bdae
ZoiloMendoza <zoilomm@hotmail.com>
2023-05-13 17:10:08 -0600

    updat

--- src/components/Search/cityCard.js
+++ src/components/Search/cityCard.js
@@ -1,52 +1,48 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/system';
 import { Card, CardMedia, CardContent, Typography, IconButton } from '@mui/material';
 import { Favorite, Add, FavoriteBorder } from '@mui/icons-material';
 import Carrusel from '../common/Carrusel';
-//import { data } from '../Add/data';
-//import Slider from 'react-slick';
-//import 'slick-carousel/slick/slick.css';
-//import 'slick-carousel/slick/slick-theme.css';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    margin: '1rem',
-    position: 'relative',
-    transition: 'transform 0.3s',
-    '&:hover': {
-      transform: 'scale(1.05)',
-    },
+const RootCard = styled(Card)(({ theme }) => ({
+  maxWidth: 345,
+  margin: '1rem',
+  position: 'relative',
+  transition: 'transform 0.3s',
+  '&:hover': {
+    transform: 'scale(1.05)',
   },
-  media: {
+}));
+
+const Media = styled(CardMedia)(({ theme }) => ({
+  width: '100%',
+  margin: 'auto',
+  transition: 'width 0.3s',
+  '&:hover': {
     width: '100%',
-    margin: 'auto',
-    transition: 'width 0.3s',
-    '&:hover': {
-      width: '100%',
-    },
   },
-  content: {
-    position: 'absolute',
-    bottom: 0,
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    color: 'white',
-    width: '100%',
-    padding: '1rem',
-    transition: 'opacity 0.3s',
-    opacity: 0,
-    '&:hover': {
-      opacity: 1,
-    },
+}));
+
+const Content = styled(CardContent)(({ theme }) => ({
+  position: 'absolute',
+  bottom: 0,
+  backgroundColor: 'rgba(0,0,0,0.6)',
+  color: 'white',
+  width: '100%',
+  padding: '1rem',
+  transition: 'opacity 0.3s',
+  opacity: 0,
+  '&:hover': {
+    opacity: 1,
   },
-  iconButton: {
-    color: '#FFF',
-  },
-});
+}));
+
+const IconButtonStyled = styled(IconButton)(({ theme }) => ({
+  color: '#FFF',
+}));
 
 const CityCard = ({ contentApi }) => {
   const [isFavorite, setIsFavorite] = useState(false);
-  const classes = useStyles();
   const [hovered, setHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -71,38 +67,25 @@ const CityCard = ({ contentApi }) => {
       <h2 style={{ marginLeft: '30px' }}>Destinos</h2>
       <Carrusel>
         {contentApi.map((item) => (
-          <Card
-            className={classes.root}
-            key={item.location_id}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <CardMedia
-              component='img'
-              className={classes.media}
-              image={item.data[0].images.small.url}
-              title={item.name}
-            />
-            <CardContent className={classes.content}>
-              {hovered && (
-                <>
-                  <Typography className={classes.title} variant='h5' gutterBottom>
-                    {item.name}
-                  </Typography>
-                  <Typography className={classes.subtitle} variant='subtitle1' gutterBottom>
-                    {item.address_obj.addres_string}
-                  </Typography>
-
-                  <IconButton className={classes.iconButton} aria-label='add' onClick={handleClick}>
-                    <Add />
-                  </IconButton>
-                  <IconButton className={classes.iconButton} aria-label='favorite' onClick={handleFavoriteClick}>
-                    {isFavorite ? <Favorite /> : <FavoriteBorder />}
-                  </IconButton>
-                </>
-              )}
-            </CardContent>
-          </Card>
+          <RootCard key={item.location_id} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Media component='img' image={item.data[0].images.small.url} title={item.name} />
+            {hovered && (
+              <Content>
+                <Typography variant='h5' gutterBottom>
+                  {item.name}
+                </Typography>
+                <Typography variant='subtitle1' gutterBottom>
+                  {item.address_obj.addres_string}
+                </Typography>
+                <IconButtonStyled aria-label='add' onClick={handleClick}>
+                  <Add />
+                </IconButtonStyled>
+                <IconButtonStyled aria-label='favorite' onClick={handleFavoriteClick}>
+                  {isFavorite ? <Favorite /> : <FavoriteBorder />}
+                </IconButtonStyled>
+              </Content>
+            )}
+          </RootCard>
         ))}
       </Carrusel>
     </>
