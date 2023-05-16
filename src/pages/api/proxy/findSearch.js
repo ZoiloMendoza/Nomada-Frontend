a917ebc65042636201c3485208c4ac076fb4ec1f
RA-1124 <alison.rangelperez@gmail.com>
2023-05-15 19:50:54 -0600

    A cambios en findsearch

--- src/pages/api/proxy/findSearch.js
+++ src/pages/api/proxy/findSearch.js
@@ -1,25 +1,23 @@
 import axios from 'axios';
 const apiKey = process.env.NEXT_PUBLIC_API_TRIPADVISOR_KEY;
 export default async function handler(req, res) {
-    const data = await getData();
-    if (Object.keys(data).length === 0) {
-      return res.status(500).json({ error: 'An error occurred while fetching data from the TripAdvisor API' });
-    }
-    return res.status(200).json(data);
-  
+  const data = await getData({ params });
+  if (Object.keys(data).length === 0) {
+    return res.status(500).json({ error: 'An error occurred while fetching data from the TripAdvisor API' });
+  }
+  return res.status(200).json(data);
 }
-export async function getData({ params }){
+export async function getData({ params }) {
   const location = `${params.destino} ${params.paisDestino}`;
-  console.log(location)
-  const apiUrl = `https://api.content.tripadvisor.com/api/v1/location/${location}/details?key=${apiKey}&language=en&currency=USD`;
-    try {
-      const response = await axios.get(apiUrl);
-      if (!response.data || !response.data.data) {
-        return {};
-      }
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while fetching data from the TripAdvisor API' });
+  console.log(location, 'getData');
+  const apiUrl = `https://api.content.tripadvisor.com/api/v1/location/search?key=${apiKey}&searchQuery=Mexico%20city%20Mx&category=geos&language=en`;
+  try {
+    const response = await axios.get(apiUrl);
+    if (!response.data) {
+      return {};
     }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
