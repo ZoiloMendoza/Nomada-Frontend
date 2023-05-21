c4af2f67cbbbbe69d62787c1efdce35839724ed1
PaolaBramlett <paolabramlett@gmail.com>
2023-05-20 18:36:41 -0600

    p. solucionando problema de <p> y <div>

--- src/components/Itinerary/AddToGroup.js
+++ src/components/Itinerary/AddToGroup.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 import { Modal, Box, Typography, TextField, Button, List, ListItem, ListItemText } from '@mui/material';
 
 const styles = {
@@ -26,8 +26,6 @@ const styles = {
   },
 };
 
-const EmailListContext = createContext();
-
 const AddToGroup = ({ openModal, closeModal }) => {
   const [email, setEmail] = useState('');
   const [emailsList, setEmailsList] = useState([]);
@@ -49,37 +47,35 @@ const AddToGroup = ({ openModal, closeModal }) => {
   };
 
   return (
-    <EmailListContext.Provider value={emailsList}>
-      <>
-        <Modal sx={styles.modal} open={openModal} onClose={handleClose}>
-          <Box sx={styles.modalContent}>
-            <Typography variant='h5'>Ingresa el email de tus acompañantes</Typography>
-            <form onSubmit={handleFormSubmit}>
-              <TextField
-                type='email'
-                value={email}
-                onChange={handleEmailChange}
-                placeholder='Ingresa email'
-                fullWidth
-                margin='normal'
-                variant='filled'
-              />
-              <Button type='submit' variant='contained' color='primary'>
-                Submit
-              </Button>
-            </form>
-            <List sx={styles.emailList}>
-              <Typography variant='h5'>Viajeros:</Typography>
-              {emailsList.map((email, index) => (
-                <ListItem key={index}>
-                  <ListItemText primary={email} />
-                </ListItem>
-              ))}
-            </List>
-          </Box>
-        </Modal>
-      </>
-    </EmailListContext.Provider>
+    <>
+      <Modal sx={styles.modal} open={openModal} onClose={handleClose}>
+        <Box sx={styles.modalContent}>
+          <Typography variant='h5'>Ingresa el email de tus acompañantes</Typography>
+          <form onSubmit={handleFormSubmit}>
+            <TextField
+              type='email'
+              value={email}
+              onChange={handleEmailChange}
+              placeholder='Ingresa email'
+              fullWidth
+              margin='normal'
+              variant='filled'
+            />
+            <Button type='submit' variant='contained' color='primary'>
+              Submit
+            </Button>
+          </form>
+          <List sx={styles.emailList}>
+            <Typography variant='h5'>Viajeros:</Typography>
+            {emailsList.map((email, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={email} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
