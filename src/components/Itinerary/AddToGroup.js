214de6c2e5632f675f830a63486eb2e665d046cb
ZoiloMendoza <zoilomm@hotmail.com>
2023-05-28 16:42:28 -0600

    update AddToGroup

--- src/components/Itinerary/AddToGroup.js
+++ src/components/Itinerary/AddToGroup.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Box, Typography, TextField, Button, List, ListItem, ListItemText } from '@mui/material';
 import axios from 'axios';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
-
+import { useRouter } from 'next/router';
+const URLRAILWAY = process.env.NEXT_PUBLIC_BACKEND;
 const styles = {
   modal: {
     position: 'fixed',
@@ -30,34 +31,67 @@ const styles = {
 };
 
 const AddToGroup = ({ openModal, closeModal }) => {
+  const router = useRouter();
+  const { id } = router.query;
   const [email, setEmail] = useState('');
   const [emailsList, setEmailsList] = useState([]);
   const [status, setStatus] = useState('');
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
-
+  useEffect(() =>{
+    try {
+      const consultaDeColaboradores = async () => {
+        const listaDeColaboradores = await axios.get(`${URLRAILWAY}/api/v1/colaboradores`)
+        if(listaDeColaboradores.status == 200){
+          console.log(listaDeColaboradores.data, 'get de colaboradores')
+          const listaDeSoloCorreos = listaDeColaboradores.data.map((colaborador)=> colaborador.email);
+          setEmailsList(listaDeSoloCorreos);
+        }
+        //setEmailsList();
+      }
+      consultaDeColaboradores()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
   const handleClose = () => {
     closeModal();
   };
 
-  const handleFormSubmit = async (e) => {
+  /*const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const resultado = await axios.post('/', { coreos: emailsList });
+      const resultado = await axios.post(`/`,{email: emailsList});
       if (resultado.status == 200) {
         setStatus('success');
-      } else setStatus('error');
+        console.log(resultado.data, 'respuesta api colaboradores')
+      } else setStatus('error'); 
     } catch (error) {
       console.log(error);
       setStatus('error');
     }
-  };
-  const guardarCorreos = (e) => {
+  };*/
+
+  const guardarCorreos = async (e) => {
     e.preventDefault();
-    if (email.trim() !== '' && !emailsList.includes(email)) {
-      setEmailsList([...emailsList, email]);
-      setEmail('');
+    try {
+        if(email.trim() !== '' && !emailsList.includes(email)) {
+            const buscarCorreo = await axios.post(`${URLRAILWAY}/api/v1/colaboradores/search`,{ email: email });
+            if(buscarCorreo.status == 200) {
+                  setStatus('success');
+                  console.log(buscarCorreo.data, 'respuesta api colaboradores')
+                  const { idUser, name, email }= buscarCorreo.data
+                  const nuevoColaborador = {viajeId: id, usuarioId: idUser, nombre: name, email: email, role: 'staff'}
+                  console.log(nuevoColaborador, 'nuevoColaborador')
+                  const registroColaborador = await axios.post(`${URLRAILWAY}/api/v1/colaboradores`, nuevoColaborador)
+                  setEmailsList([...emailsList, email]);
+                  setEmail('');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setStatus('error');
     }
   };
 
@@ -70,7 +104,7 @@ const AddToGroup = ({ openModal, closeModal }) => {
             {status == 'error' && <Alert severity='error'>Error </Alert>}
           </Stack>
           <Typography variant='h5'>Ingresa el email de tus acompañantes</Typography>
-          <form onSubmit={handleFormSubmit}>
+          <form >
             <TextField
               type='email'
               value={email}
