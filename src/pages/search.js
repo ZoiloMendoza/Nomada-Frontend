e106b2807ccecca1d8acdc10d917bc9495059966
ZoiloMendoza <zoilomm@hotmail.com>
2023-06-09 02:33:02 -0600

    update useEffect

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,17 +5,40 @@ import Box from '@mui/material/Box';
 import CityCard from '@/components/Search/cityCard';
 import ActivityCard from '@/components/Search/activityCard';
 
-export default function Search({ contentRestaurant, contentDestino, contentActividades }) {
-  //console.log(contentRestaurant);
-  //console.log(contentDestino);
-  console.log(contentActividades, 'contentActividades');
+export default function Search() {
+  const [restaurantData, setRestaurantData] = useState(null);
+  const [destinoData, setDestinoData] = useState(null);
+  const [actividadesData, setActividadesData] = useState(null);
+
+useEffect(() => {
+  const latitude = '19.7059504';
+  const longitude = '-101.1949825';
+  const url = `/api/proxy/search/${latitude}/${longitude}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      setRestaurantData(data.contentRestaurant);
+      setDestinoData(data.contentDestino);
+      setActividadesData(data.contentActividades);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}, []);
   return (
     <>
       <SearchBar />
 
-      <Box>{<CityCard contentApi={contentDestino} />}</Box>
-      <Box>{<RestaurantCard restaurantData={contentRestaurant} />}</Box>
-      <Box>{<ActivityCard activityData={contentActividades} />}</Box>
+      <Box>
+        {destinoData && <CityCard contentApi={destinoData} />}
+      </Box>
+      <Box>
+        {restaurantData && <RestaurantCard restaurantData={restaurantData} />}
+      </Box>
+      <Box>
+        {actividadesData && <ActivityCard activityData={actividadesData} />}
+      </Box>
 
       <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <MapButton />
