b9e72bfd11e764303d45f8c9084f895218dbb9a5
RA-1124 <alison.rangelperez@gmail.com>
2023-07-07 17:05:21 -0600

    A cambios para deshabilitar botom registro hasta que coincida contrasena

--- src/components/Registro/formulario.js
+++ src/components/Registro/formulario.js
@@ -1,6 +1,6 @@
-import { Box, TextField, Grid, Card, CardContent, Typography } from '@mui/material';
+import { Box, TextField, Grid, Card, CardContent, Typography, Button } from '@mui/material';
 //import CheckboxFormulario from './CheckboxFormulario';
-import ButtonForm from './ButtonForm';
+//import ButtonForm from './ButtonForm';
 import { useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
@@ -25,31 +25,36 @@ const style = {
   },
 };
 
+function ButtonForm({ size, onClick, disabled }) {
+  return (
+    <Button variant='contained' size={size} color='primary' onClick={onClick} disabled={disabled}>
+      Crear cuenta
+    </Button>
+  );
+}
+
 function Formulario() {
   const router = useRouter();
-  const [confirmarPassword, setConfirmarPassword] = useState({
-    confirmar: '',
-  });
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     role: 'admin',
     password: '',
+    confirmar: '',
   });
-  const { name, email, password } = formData;
   const [status, setStatus] = useState('');
 
   const handleOnChange = (e) => {
     console.log([e.target.name], e.target.value);
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  const handleOnChangeConfirmarPassword = (e) => {
-    console.log([e.target.name], e.target.value);
-    setConfirmarPassword({ ...confirmarPassword, [e.target.name]: e.target.value });
-  };
+  //const handleOnChangeConfirmarPassword = (e) => {
+  //  console.log([e.target.name], e.target.value);
+  //  setConfirmarPassword({ ...confirmarPassword, [e.target.name]: e.target.value });
+  //};
 
-  const handleSubmit = async (name, email, password) => {
-    if (password !== confirmarPassword.confirmar) {
+  const handleSubmit = async (name, email, password, confirmar) => {
+    if (password !== confirmar) {
       setStatus('error');
       return;
     }
@@ -57,7 +62,6 @@ function Formulario() {
     try {
       const userPost = await axios.post(`${URLRAILWAY}/api/v1/signup`, formData);
       console.log('statusCode', userPost.status);
-      console;
       if (userPost.status == 201) {
         console.log(userPost);
         console.log('Usuario creado exitosamente');
@@ -82,6 +86,13 @@ function Formulario() {
     }
   };
 
+  const isFormComplete = () =>
+    formData.name !== '' &&
+    formData.email !== '' &&
+    formData.password !== '' &&
+    formData.confirmar !== '' &&
+    formData.password === formData.confirmar;
+
   return (
     <div className='Formulario'>
       <Typography align='center' variant='h5' sx={{ paddingTop: '5vh' }}>
@@ -91,7 +102,7 @@ function Formulario() {
 
       <Box sx={style.formulario}>
         <Grid container direction='row' spacing={5}>
-          <Grid item xs={18} sm={18} md={18} lg={18} xl={18}>
+          <Grid item xs={12}>
             <Card>
               <Stack sx={{ width: '100%' }} autoHideDuration={5000} spacing={2}>
                 {status === 'success' && (
@@ -103,7 +114,7 @@ function Formulario() {
                 {status === 'error' && (
                   <Alert severity='error'>
                     <AlertTitle>Error</AlertTitle>
-                    Ocurrió un error, las contraseñas no coinciden.
+                    Ocurrió un error, verificar que las contraseñas coincidan.
                   </Alert>
                 )}
               </Stack>
@@ -114,76 +125,68 @@ function Formulario() {
                   label='usuario'
                   type='text'
                   name='name'
-                  value={formData.name || ''}
+                  value={formData.name}
+                  onChange={handleOnChange}
+                  margin='dense'
+                  fullWidth
+                  sx={{ fontFamily: 'Inter, sans-serif' }}
+                />
+                <TextField
+                  variant='filled'
+                  error={false}
+                  label='correo electrónico'
+                  type='text'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleOnChange}
+                  margin='dense'
+                  fullWidth
+                  sx={{ fontFamily: 'Inter, sans-serif' }}
+                />
+                <TextField
+                  sx={{ fontFamily: 'Inter, sans-serif' }}
+                  variant='filled'
+                  error={false}
+                  label='contraseña'
+                  type='password'
+                  name='password'
+                  helperText='Debe contener minimo 8 digitos, 1 mayuscula y 1 carácter especial.'
+                  value={formData.password}
+                  onChange={handleOnChange}
+                  margin='dense'
+                  fullWidth
+                  sx={{ fontFamily: 'Inter, sans-serif' }}
+                />
+                <TextField
+                  sx={{ fontFamily: 'Inter, sans-serif' }}
+                  variant='filled'
+                  error={false}
+                  label='confirmar contraseña'
+                  type='password'
+                  name='confirmar'
+                  helperText='Debe contener minimo 8 digitos, 1 mayuscula y 1 carácter especial.'
+                  value={formData.confirmar}
                   onChange={handleOnChange}
                   margin='dense'
                   fullWidth
                   sx={{ fontFamily: 'Inter, sans-serif' }}
                 />
               </CardContent>
-              <Grid item xs={18} sm={18} md={18} lg={18} xl={18}>
-                <Card>
-                  <CardContent>
-                    <TextField
-                      variant='filled'
-                      error={false}
-                      label='correo electrónico'
-                      type='text'
-                      name='email'
-                      value={formData.email || ''}
-                      onChange={handleOnChange}
-                      margin='dense'
-                      fullWidth
-                      sx={{ fontFamily: 'Inter, sans-serif' }}
-                    />
-                  </CardContent>
-                  <Grid item xs={18} sm={18} md={18} lg={18} xl={18}>
-                    <Card>
-                      <CardContent>
-                        <TextField
-                          sx={{ fontFamily: 'Inter, sans-serif' }}
-                          variant='filled'
-                          error={false}
-                          label='contraseña'
-                          type='password'
-                          name='password'
-                          value={formData.password || ''}
-                          onChange={handleOnChange}
-                          margin='dense'
-                          fullWidth
-                        />
-                      </CardContent>
-                      <Grid item xs={18} sm={18} md={18} lg={18} xl={18}>
-                        <Card>
-                          <CardContent>
-                            <TextField
-                              sx={{ fontFamily: 'Inter, sans-serif' }}
-                              variant='filled'
-                              error={false}
-                              label='confirmar contraseña'
-                              type='password'
-                              name='confirmar'
-                              value={confirmarPassword.confirmar || ''}
-                              onChange={handleOnChangeConfirmarPassword}
-                              margin='dense'
-                              fullWidth
-                            />
-                          </CardContent>
-
-                          <p>¿Ya tienes una cuenta?</p>
-                          <Link legacyBehavior href='/login'>
-                            <a style={{ color: 'blue', cursor: 'pointer' }}>Inicia sesión</a>
-                          </Link>
-
-                          <Grid my={4} justifyContent='center' direction='row' container>
-                            <ButtonForm size='medium' onClick={() => handleSubmit(name, email, password)} />
-                          </Grid>
-                        </Card>
-                      </Grid>
-                    </Card>
-                  </Grid>
-                </Card>
-              </Grid>
+              <CardContent>
+                <p>¿Ya tienes una cuenta?</p>
+                <Link legacyBehavior href='/login'>
+                  <a style={{ color: 'blue', cursor: 'pointer' }}>Inicia sesión</a>
+                </Link>
+              </CardContent>
+              <CardContent>
+                <Grid my={4} justifyContent='center' direction='row' container>
+                  <ButtonForm
+                    size='medium'
+                    onClick={() => handleSubmit(formData.name, formData.email, formData.password, formData.confirmar)}
+                    disabled={!isFormComplete()}
+                  />
+                </Grid>
+              </CardContent>
             </Card>
           </Grid>
         </Grid>
